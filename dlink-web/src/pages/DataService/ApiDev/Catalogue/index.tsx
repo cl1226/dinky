39e92d365ee68c050dcc0a1d48180e48ef40a612
firefishzz <371152107@qq.com>
2023-05-15 19:16:24 +0800

    api目录

--- dlink-web/src/pages/DataService/ApiDev/Catalogue/index.tsx
+++ dlink-web/src/pages/DataService/ApiDev/Catalogue/index.tsx
@@ -1,8 +1,10 @@
 import React, { useCallback, useEffect, useState } from 'react'
+import { connect } from 'umi'
 import styles from './index.less'
 import { Card, Col, Form, Row } from 'antd'
 import { PageContainer } from '@ant-design/pro-layout'
 import CatalogueTree from './components/CatalogueTree'
+import { StateType } from './model'
 const ApiCatalogue: React.FC<{}> = (props: any) => {
   return (
     <PageContainer title={false}>
@@ -13,4 +15,6 @@ const ApiCatalogue: React.FC<{}> = (props: any) => {
   )
 }
 
-export default ApiCatalogue
+export default connect(({ Catalogue }: { Catalogue: StateType }) => ({
+
+}))(ApiCatalogue)
