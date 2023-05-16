608a7ba7e86397cda7e0502de1b776b45ad30719
firefishzz <371152107@qq.com>
2023-05-16 15:16:08 +0800

    滚动条样式

--- dlink-web/src/components/XFlow/json-form.tsx
+++ dlink-web/src/components/XFlow/json-form.tsx
@@ -6,6 +6,7 @@ import type { RadioChangeEvent } from 'antd'
 import { Cron } from '@/components/Cron'
 import { NS_CANVAS_FORM } from './config-model-service'
 import { IMeta, ESchedulerType, getJsonCron } from './service'
+import { Scrollbars } from 'react-custom-scrollbars'
 
 const RangePicker: any = DatePicker.RangePicker
 
@@ -28,6 +29,7 @@ export namespace CustomJsonForm {
     const [canvasForm] = Form.useForm()
     const [baseInfo, setBaseInfo] = useState<IMeta>({ flowId: '' })
     const [cycleVisible, setCycleVisible] = useState<boolean>(false)
+    const sref: any = React.createRef<Scrollbars>()
 
     const registerModel = async () => {
       if (!modelService.findDeferredModel(NS_CANVAS_FORM.id)) {
@@ -101,69 +103,71 @@ export namespace CustomJsonForm {
       <div className="custom-form-component">
         <div className="info-title">作业信息</div>
         <div className="info-wrap">
-          <div className="info-content">
-            <Descriptions title="基本信息" column={1}>
-              <Descriptions.Item label="作业ID">{baseInfo.flowId}</Descriptions.Item>
-              <Descriptions.Item label="作业名称">{baseInfo.flowName}</Descriptions.Item>
-              <Descriptions.Item label="作业状态">{baseInfo.status}</Descriptions.Item>
-            </Descriptions>
-            <Form
-              name="canvasForm"
-              form={canvasForm}
-              initialValues={{
-                timezoneId: 'Asia/Shanghai',
-                timerange: [moment(), moment().add(100, 'y')],
-                schedulerType: ESchedulerType.SINGLE,
-              }}
-            >
-              <Descriptions title="调度配置" column={1} layout="vertical">
-                <Descriptions.Item label="调度方式">
-                  <Form.Item name="schedulerType" style={{ width: '100%', marginBottom: 0 }}>
-                    <Radio.Group
-                      onChange={(e: RadioChangeEvent) => {
-                        setCycleVisible(e.target.value === ESchedulerType.CYCLE)
-                      }}
-                    >
-                      <Radio value={ESchedulerType.SINGLE}>单次调度</Radio>
-                      <Radio value={ESchedulerType.CYCLE}>周期调度</Radio>
-                    </Radio.Group>
-                  </Form.Item>
-                </Descriptions.Item>
-
-                {cycleVisible ? (
-                  <>
-                    <Descriptions.Item key="timerange" label="起止时间">
-                      <Form.Item
-                        name="timerange"
-                        rules={[{ required: true, message: '请选择起止时间！' }]}
-                        style={{ width: '100%' }}
-                      >
-                        <RangePicker showTime />
-                      </Form.Item>
-                    </Descriptions.Item>
-                    <Descriptions.Item key="crontab" label="定时">
-                      <Form.Item
-                        name="crontab"
-                        rules={[{ required: true, message: '请选择定时！' }]}
-                        style={{ width: '100%' }}
-                      >
-                        <Cron />
-                      </Form.Item>
-                    </Descriptions.Item>
-                    <Descriptions.Item key="timezoneId" label="时区">
-                      <Form.Item
-                        name="timezoneId"
-                        rules={[{ required: true, message: '请选择时区！' }]}
-                        style={{ width: '100%' }}
-                      >
-                        <Input readOnly />
-                      </Form.Item>
-                    </Descriptions.Item>
-                  </>
-                ) : null}
+          <Scrollbars style={{ height: '100%' }} ref={sref}>
+            <div className="info-content">
+              <Descriptions title="基本信息" column={1}>
+                <Descriptions.Item label="作业ID">{baseInfo.flowId}</Descriptions.Item>
+                <Descriptions.Item label="作业名称">{baseInfo.flowName}</Descriptions.Item>
+                <Descriptions.Item label="作业状态">{baseInfo.status}</Descriptions.Item>
               </Descriptions>
-            </Form>
-          </div>
+              <Form
+                name="canvasForm"
+                form={canvasForm}
+                initialValues={{
+                  timezoneId: 'Asia/Shanghai',
+                  timerange: [moment(), moment().add(100, 'y')],
+                  schedulerType: ESchedulerType.SINGLE,
+                }}
+              >
+                <Descriptions title="调度配置" column={1} layout="vertical">
+                  <Descriptions.Item label="调度方式">
+                    <Form.Item name="schedulerType" style={{ width: '100%', marginBottom: 0 }}>
+                      <Radio.Group
+                        onChange={(e: RadioChangeEvent) => {
+                          setCycleVisible(e.target.value === ESchedulerType.CYCLE)
+                        }}
+                      >
+                        <Radio value={ESchedulerType.SINGLE}>单次调度</Radio>
+                        <Radio value={ESchedulerType.CYCLE}>周期调度</Radio>
+                      </Radio.Group>
+                    </Form.Item>
+                  </Descriptions.Item>
+
+                  {cycleVisible ? (
+                    <>
+                      <Descriptions.Item key="timerange" label="起止时间">
+                        <Form.Item
+                          name="timerange"
+                          rules={[{ required: true, message: '请选择起止时间！' }]}
+                          style={{ width: '100%' }}
+                        >
+                          <RangePicker showTime />
+                        </Form.Item>
+                      </Descriptions.Item>
+                      <Descriptions.Item key="crontab" label="定时">
+                        <Form.Item
+                          name="crontab"
+                          rules={[{ required: true, message: '请选择定时！' }]}
+                          style={{ width: '100%' }}
+                        >
+                          <Cron />
+                        </Form.Item>
+                      </Descriptions.Item>
+                      <Descriptions.Item key="timezoneId" label="时区">
+                        <Form.Item
+                          name="timezoneId"
+                          rules={[{ required: true, message: '请选择时区！' }]}
+                          style={{ width: '100%' }}
+                        >
+                          <Input readOnly />
+                        </Form.Item>
+                      </Descriptions.Item>
+                    </>
+                  ) : null}
+                </Descriptions>
+              </Form>
+            </div>
+          </Scrollbars>
         </div>
       </div>
     )
