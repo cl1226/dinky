a3e8e2610908a35e0b58644311440a89afbcffa5
firefishzz <371152107@qq.com>
2023-05-26 09:54:24 +0800

    feat: api detail

--- dlink-web/src/pages/DataService/ApiDev/Detail/index.tsx
+++ dlink-web/src/pages/DataService/ApiDev/Detail/index.tsx
@@ -1,7 +1,8 @@
 import React, { useRef, useState, useEffect } from 'react'
 import styles from './index.less'
 import { PageContainer } from '@ant-design/pro-layout'
-import { Descriptions, Row, Col, Typography, Tabs } from 'antd'
+import { Descriptions, Row, Col, Typography, Tabs, Input, Table } from 'antd'
+import { DesktopOutlined, ArrowRightOutlined, HddOutlined } from '@ant-design/icons'
 import { history, useParams } from 'umi'
 
 import { Scrollbars } from 'react-custom-scrollbars'
@@ -9,7 +10,7 @@ import { requestApiDetail } from '@/pages/DataService/ApiDev/Edit/service'
 
 import { CODE } from '@/components/Common/crud'
 import { EAccessType, EContentType, EAuthType } from '@/utils/enum'
-const { Paragraph } = Typography
+import { EDataType } from '@/pages/DataService/ApiDev/Create/components/Parameters'
 
 const ApiDetail: React.FC<{}> = (props: any) => {
   const sref: any = React.createRef<Scrollbars>()
@@ -36,6 +37,140 @@ const ApiDetail: React.FC<{}> = (props: any) => {
       })
   }, [])
 
+  const getDispatchInfo = () => {
+    const getDataSource = () => {
+      return JSON.parse(detailInfo?.params || '[]').map((item, index) => ({
+        ...item,
+        tempId: index,
+      }))
+    }
+    const columns = [
+      {
+        title: '参数名',
+        dataIndex: 'name',
+        key: 'name',
+        width: 150,
+      },
+      {
+        title: '参数类型',
+        dataIndex: 'type',
+        key: 'type',
+        width: 150,
+        render: (cellValue, record) => {
+          return EDataType[cellValue]
+        },
+      },
+      {
+        title: '入参位置',
+        key: 'position',
+        width: 150,
+        render: (cellValue, record) => 'QUERY',
+      },
+      {
+        title: '描述',
+        dataIndex: 'description',
+        key: 'description',
+        width: 150,
+      },
+    ]
+    return (
+      <div className={styles['tab-wrap']}>
+        <div className="description-item">
+          <div className="description-label">基础定义</div>
+          <div className="description-content base-define">
+            <div className="icon-box">
+              <DesktopOutlined style={{ fontSize: 32, marginBottom: 10 }} />
+              <span>客户端</span>
+            </div>
+            <ArrowRightOutlined style={{ fontSize: 40, color: '#ccc' }} />
+            <div className="info-box">
+              <Descriptions
+                title="API 请求"
+                column={1}
+                labelStyle={{ width: 100, color: '#8a8e99' }}
+              >
+                <Descriptions.Item label="域名">{detailInfo.domain || '-'}</Descriptions.Item>
+                <Descriptions.Item label="请求路径">{detailInfo.path || '-'}</Descriptions.Item>
+                <Descriptions.Item label="Content-Type">{detailInfo.contentType}</Descriptions.Item>
+              </Descriptions>
+            </div>
+            <ArrowRightOutlined style={{ fontSize: 40, color: '#ccc' }} />
+            <div className="info-box">
+              <Descriptions title="数据源" column={1} labelStyle={{ width: 100, color: '#8a8e99' }}>
+                <Descriptions.Item label="数据源类型">
+                  {detailInfo.datasourceType || '-'}
+                </Descriptions.Item>
+                <Descriptions.Item label="数据库">
+                  {detailInfo.datasourceDb || '-'}
+                </Descriptions.Item>
+                <Descriptions.Item label="sql语句">
+                  <Typography.Text
+                    style={{ width: 150 }}
+                    ellipsis={{ tooltip: detailInfo.segment }}
+                  >
+                    {detailInfo.segment}
+                  </Typography.Text>
+                </Descriptions.Item>
+              </Descriptions>
+            </div>
+            <ArrowRightOutlined style={{ fontSize: 40, color: '#ccc' }} />
+
+            <div className="icon-box">
+              <HddOutlined style={{ fontSize: 32, marginBottom: 10 }} />
+            </div>
+          </div>
+        </div>
+
+        <div className="description-item">
+          <div className="description-label">入参定义</div>
+          <div className="description-content">
+            <Table
+              style={{ width: '100%' }}
+              rowKey="tempId"
+              size="small"
+              dataSource={getDataSource()}
+              columns={columns}
+              pagination={false}
+            />
+          </div>
+        </div>
+
+        <div className="description-item">
+          <div className="description-label">成功响应示例</div>
+          <div className="description-content">
+            <Input.TextArea
+              style={{ resize: 'none' }}
+              rows={6}
+              readOnly
+              value={`{
+                  "code": 0,
+                  "datas": {},
+                  "msg": "Interface request successful!",
+                  "time": "2023-05-26 09:50:53"
+                }`}
+            ></Input.TextArea>
+          </div>
+        </div>
+
+        <div className="description-item">
+          <div className="description-label">失败响应示例</div>
+          <div className="description-content">
+            <Input.TextArea
+              style={{ resize: 'none' }}
+              rows={6}
+              readOnly
+              value={`{
+                "code": 1,
+                "datas": {},
+                "msg": "Interface request failed",
+                "time": "2023-05-26 09:51:42"
+            }`}
+            ></Input.TextArea>
+          </div>
+        </div>
+      </div>
+    )
+  }
   return (
     <PageContainer title={pageTitle || false} onBack={() => history.goBack()} loading={loading}>
       <Scrollbars
@@ -61,8 +196,10 @@ const ApiDetail: React.FC<{}> = (props: any) => {
                   {detailInfo.updateTime || detailInfo.createTime || '-'}
                 </Descriptions.Item>
                 <Descriptions.Item label="URL">
-                  <Typography.Text copyable={{ text: detailInfo.absolutePath }}>
-                    {detailInfo.absolutePath}
+                  <Typography.Text
+                    copyable={{ text: `${detailInfo.domain}${detailInfo.absolutePath}` }}
+                  >
+                    {`${detailInfo.domain}${detailInfo.absolutePath}`}
                   </Typography.Text>
                 </Descriptions.Item>
                 <Descriptions.Item label="描述">{detailInfo.description || '-'}</Descriptions.Item>
@@ -70,6 +207,7 @@ const ApiDetail: React.FC<{}> = (props: any) => {
             </Col>
           </Row>
           <Tabs
+            className={styles['tabs-wrap']}
             defaultActiveKey="1"
             onChange={onChange}
             type="card"
@@ -77,7 +215,7 @@ const ApiDetail: React.FC<{}> = (props: any) => {
               {
                 label: '调用信息',
                 key: '1',
-                children: <div>调用信息</div>,
+                children: getDispatchInfo(),
               },
               {
                 label: '授权信息',
