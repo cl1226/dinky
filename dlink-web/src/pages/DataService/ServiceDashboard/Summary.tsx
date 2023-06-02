953a1863dc2ef6c9a6bd64862304dec092ce9fa8
firefishzz <371152107@qq.com>
2023-06-02 11:41:56 +0800

    feat: summary

--- dlink-web/src/pages/DataService/ServiceDashboard/Summary.tsx
+++ dlink-web/src/pages/DataService/ServiceDashboard/Summary.tsx
@@ -1,15 +1,33 @@
 import { useEffect, useRef, useState, useMemo, useCallback } from 'react'
 import styles from './index.less'
 import { Col, Row, DatePicker, Statistic, Card, Alert, Space } from 'antd'
-import type { DatePickerProps } from 'antd'
 import * as echarts from 'echarts'
+import {
+  getSummaryCount,
+  getTop5Api,
+  getTop5Duration,
+  requestSummary,
+} from '@/pages/DataService/ServiceDashboard/service'
+import moment from 'moment'
+import type { Moment } from 'moment'
+
+type RangeValue = [Moment, Moment]
 
 const RangePicker: any = DatePicker.RangePicker
-
+const defaultSummary = {
+  onlineNum: 0,
+  offlineNum: 0,
+  successNum: 0,
+  failNum: 0,
+  beginDate: '',
+  endDate: '',
+}
 export default () => {
-  const onChange: DatePickerProps['onChange'] = (date, dateString) => {
-    console.log(date, dateString)
-  }
+  const [dateRange, setDateRange] = useState<RangeValue>([moment().subtract(6, 'day'), moment()])
+  const [apiVisits, setApiVisits] = useState<any>({ x: [], success: [], fail: [] })
+  const [top5Api, setTop5Api] = useState<any>({ x: [], y: [] })
+  const [top5Duration, setTop5Duration] = useState<any>({ x: [], y: [] })
+  const [summary, setSummary] = useState<any>(defaultSummary)
 
   const ApiVisitsChart = (props) => {
     const { chartData } = props
@@ -40,9 +58,20 @@ export default () => {
         },
         series: [
           {
-            data: chartData.y,
+            name: '成功',
+            data: chartData.success,
             type: 'line',
-            smooth: true,
+            itemStyle: {
+              color: 'rgb(11, 130, 53)',
+            },
+          },
+          {
+            name: '失败',
+            data: chartData.fail,
+            type: 'line',
+            itemStyle: {
+              color: 'rgb(255, 77, 79)',
+            },
           },
         ],
       }
@@ -97,51 +126,82 @@ export default () => {
 
     return <div style={{ width: '100%', height: 380 }} ref={chartsRef}></div>
   }
+
+  const initChartsData = ([beginDate, endDate]) => {
+    const params = {
+      beginDate: beginDate.format('YYYY-MM-DD'),
+      endDate: endDate.add(1, 'day').format('YYYY-MM-DD'),
+    }
+
+    Promise.all([
+      getSummaryCount(params),
+      getTop5Api(params),
+      getTop5Duration(params),
+      requestSummary(),
+    ]).then(([apiVisitsRes, top5ApiRes, top5DurationRes, summaryRes]) => {
+      console.log(apiVisitsRes, top5ApiRes, top5DurationRes, summaryRes)
+      setApiVisits(apiVisitsRes)
+      setTop5Api(top5ApiRes)
+      setTop5Duration(top5DurationRes)
+      setSummary(summaryRes.datas || defaultSummary)
+    })
+  }
+  useEffect(() => {
+    initChartsData(dateRange)
+  }, [])
   return (
     <>
-      <RangePicker onChange={onChange} />
+      <RangePicker
+        allowClear={false}
+        value={dateRange}
+        onChange={(date) => {
+          setDateRange(date)
+        }}
+        onOpenChange={(bool) => {
+          !bool && initChartsData(dateRange)
+        }}
+      />
       <Row style={{ marginTop: 10 }}>
         <Col span={18}>
           <div className={styles['title']}>Api访问量</div>
-          <ApiVisitsChart
-            chartData={{
-              x: ['12/30', '12/31', '1/1', '1/2', '1/3', '1/4', '1/5'],
-              y: [820, 932, 901, 934, 1290, 1330, 1320],
-            }}
-          ></ApiVisitsChart>
+          {useMemo(() => {
+            return <ApiVisitsChart chartData={apiVisits}></ApiVisitsChart>
+          }, [apiVisits])}
         </Col>
         <Col offset={1} span={5}>
           <div className={styles['title']}>汇总信息</div>
           <div className={styles['summary-card']}>
             <div className="summary-top">
               <div className="statistic-box">
-                <div className="statistic-value">58</div>
+                <div className="statistic-value">{summary.onlineNum}</div>
                 <div className="statistic-text">已发布</div>
               </div>
               <div className="divider-line"></div>
               <div className="statistic-box">
-                <div className="statistic-value">58</div>
+                <div className="statistic-value">{summary.offlineNum}</div>
                 <div className="statistic-text">开发中</div>
               </div>
             </div>
             <div className="summary-data">
               <div className="all-calls">
-                <span className="title">5.25 ~ 5.31 总调用</span>
-                <span className="value">0</span>
+                <span className="title">{`${moment(summary.beginDate).format('MM.DD')} ~ ${moment(
+                  summary.endDate,
+                ).format('MM.DD')} 总调用`}</span>
+                <span className="value">{summary.successNum + summary.failNum}</span>
               </div>
               <Row>
                 <Col span={12}>
                   <Statistic
                     title="成功"
                     valueStyle={{ color: '#3f8600' }}
-                    value={112893}
+                    value={summary.successNum}
                   ></Statistic>
                 </Col>
                 <Col span={12}>
                   <Statistic
                     title="失败"
                     valueStyle={{ color: '#cf1322' }}
-                    value={112893}
+                    value={summary.failNum}
                   ></Statistic>
                 </Col>
               </Row>
@@ -152,22 +212,18 @@ export default () => {
       <Row style={{ marginTop: 10 }}>
         <Col span={11}>
           <div className={styles['title']}>成功调用次数</div>
-          <RankBarChart
-            chartData={{
-              x: ['1/1', '1/2', '1/3', '1/4', '1/5'],
-              y: [901, 934, 1290, 1330, 1320],
-            }}
-          ></RankBarChart>
+
+          {useMemo(() => {
+            return <RankBarChart chartData={top5Api}></RankBarChart>
+          }, [top5Api])}
         </Col>
 
         <Col offset={2} span={11}>
-          <div className={styles['title']}>平均访问时长</div>
-          <RankBarChart
-            chartData={{
-              x: ['1/1', '1/2', '1/3', '1/4', '1/5'],
-              y: [901, 934, 1290, 1330, 1320],
-            }}
-          ></RankBarChart>
+          <div className={styles['title']}>平均访问时长(ms)</div>
+
+          {useMemo(() => {
+            return <RankBarChart chartData={top5Duration}></RankBarChart>
+          }, [top5Duration])}
         </Col>
       </Row>
     </>
